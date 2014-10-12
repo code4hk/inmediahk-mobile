5e7d16d401a6bfd17dd31a65e21268695cecb594
Allen Chan <allen517@gmail.com>
2014-10-13 05:18:29 +0800

    Now can display all images on single page

--- app/src/main/java/net/inmediahk/reader/ItemDetailFragment.java
+++ app/src/main/java/net/inmediahk/reader/ItemDetailFragment.java
@@ -1,5 +1,11 @@
 package net.inmediahk.reader;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LevelListDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
@@ -13,6 +19,12 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import net.inmediahk.reader.Model.FeedItem;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -30,6 +42,20 @@ public class ItemDetailFragment extends Fragment {
     public static final String ARG_FEED = "feed_item";
 //    private FeedManager mFeedManager;
     private FeedItem mItem;
+    private TextView mTv;
+    private Html.ImageGetter imgGetter = new Html.ImageGetter() {
+        @Override
+        public Drawable getDrawable(String source) {
+            LevelListDrawable drawable = new LevelListDrawable();
+            Drawable empty = getResources().getDrawable(R.drawable.ic_launcher);
+            drawable.addLevel(0, 0, empty);
+            drawable.setBounds(0, 0, empty.getIntrinsicWidth(), empty.getIntrinsicHeight());
+
+            new LoadImage().execute(source, drawable);
+
+            return drawable;
+        }
+    };
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -50,20 +76,61 @@ public class ItemDetailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        mTv = (TextView) rootView.findViewById(R.id.txtDesc);
 
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.txtTitle)).setText(mItem.getTitle());
             ((TextView) rootView.findViewById(R.id.txtAuthor)).setText("文：" + mItem.getCreator());
             ((TextView) rootView.findViewById(R.id.txtDate)).setText(mItem.getDate());
-            ((TextView) rootView.findViewById(R.id.txtDesc)).setText(Html.fromHtml(mItem.getContent()));
+
             if (!mItem.getImageUrl().equals(""))
                 Picasso.with(getActivity()).load(mItem.getImageUrl()).into(((ImageView) rootView.findViewById(R.id.imageView)));
-            ((TextView) rootView.findViewById(R.id.txtDesc)).setMovementMethod(LinkMovementMethod.getInstance());
+
+            mTv.setText(Html.fromHtml(mItem.getContent(), imgGetter, null));
+            mTv.setMovementMethod(LinkMovementMethod.getInstance());
         }
 
         return rootView;
     }
 
+    class LoadImage extends AsyncTask<Object, Void, Bitmap> {
+
+        private LevelListDrawable mDrawable;
+
+        @Override
+        protected Bitmap doInBackground(Object... params) {
+            String source = (String) params[0];
+            mDrawable = (LevelListDrawable) params[1];
+            try {
+                InputStream is = new URL(source).openStream();
+                return BitmapFactory.decodeStream(is);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                BitmapDrawable d = new BitmapDrawable(bitmap);
+
+                int width = mTv.getWidth() < bitmap.getWidth() ? mTv.getWidth() : bitmap.getWidth();
+                int height = bitmap.getHeight() * width / bitmap.getWidth();
+
+                mDrawable.addLevel(1, 1, d);
+                mDrawable.setBounds(0, 0, width, height);
+                mDrawable.setLevel(1);
+
+                CharSequence t = mTv.getText();
+                mTv.setText(t);
+            }
+        }
+    }
 }
